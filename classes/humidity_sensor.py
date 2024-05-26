5ab1cefa719db23ca6846360f705cfd3827c9651
Tilman <tlf.koenig@gmail.com>
2024-05-26 21:37:29 +0200

    sensor class update

--- classes/humidity_sensor.py
+++ classes/humidity_sensor.py
@@ -4,6 +4,7 @@ import busio
 import adafruit_ads1x15.ads1115 as ads
 from adafruit_ads1x15.analog_in import AnalogIn
 
+
 i2c = busio.I2C(board.SCL, board.SDA)
 adc = ads.ADS1115(i2c)
 
@@ -12,7 +13,7 @@ class humidity_sensor:
     def __init__(self, sensor_id, adc_channel, unit = None, ):
         self.sensor_id = sensor_id
         self.unit = "% air moisture"
-        self.adc_channel = AnalogIn(eval('adc, ADS.' + str(adc_channel)))
+        self.adc_channel = AnalogIn(eval('adc, ads.' + str(adc_channel)))
         self.sensor_data = []
 
     def read(self):
