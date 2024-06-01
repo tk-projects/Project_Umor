917de64dae92581d19f930af2fb6fba65954b9dd
Tilman <tlf.koenig@gmail.com>
2024-06-01 17:26:38 +0200

    sdasd

--- sensor_main.py
+++ sensor_main.py
@@ -4,15 +4,19 @@ def sensor_main():
     import json
     
     sensors_file_path = os.path.join(os.getcwd() + "/bin/sensors.json")
+    print("Loading Sensor Data from: ", sensors_file_path)
 
     with open(sensors_file_path, "r") as json_file:
         sensor_json = json.load(json_file)
 
+    print("Loading succesful")
+
     sensors = {}
 
     for sensor_name, sensor_json in sensor_json.items():
-        sensors[sensor_name] = humidity_sensor(sensor_json["sensor_id"], sensor_json["adc_channel"], sensor_json["name"], sensor_json["unit"], sensor_json["min_offset"],sensor_json["max_offset"])  # Initialize sensor directly from dictionary
+        sensors[sensor_name] = humidity_sensor(sensor_json["sensor_id"], sensor_json["adc_channel"], sensor_json["name"], sensor_json["unit"], sensor_json["min_offset"],sensor_json["max_offset"])
     
-    print(sensors)
+    print("Sensor data: ", sensors)
+
     return sensors
     
