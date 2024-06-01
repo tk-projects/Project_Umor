90d0563a5e451b88837b3b2feadde6b34cce4076
Tilman <tlf.koenig@gmail.com>
2024-06-01 17:54:03 +0200

    sdas

--- sensor_main.py
+++ sensor_main.py
@@ -11,11 +11,11 @@ with open(sensors_file_path, "r") as json_file:
 
 print("________________________________________________________________________________________________________")
 print("________________________________________________________________________________________________________\n")
-print("Initializing")
+print("Importing Sensor Data:")
 dauer = 12
 for _ in range(dauer):
     print(".", end="", flush=True)
-    time.sleep(0.04)
+    time.sleep(0.06)
 
 print("\n")
 
@@ -24,7 +24,7 @@ sensors = {}
 
 for sensor_name, sensor_json in sensor_json.items():
     sensors[sensor_name] = humidity_sensor(sensor_json["sensor_id"], sensor_json["adc_channel"], sensor_json["name"], sensor_json["unit"], sensor_json["max_calibration_value"], sensor_json["min_calibration_value"])
-    print(" • Imported: Sensor",sensor_json["sensor_id"], "| Channel:", sensor_json["adc_channel"], "| max value: ", sensor_json["max_calibration_value"], "| min value:",sensor_json["min_calibration_value"],"|")
+    print("  • Sensor",sensor_json["sensor_id"], "| Channel:", sensor_json["adc_channel"], "| max value: ", sensor_json["max_calibration_value"], "| min value:",sensor_json["min_calibration_value"],"|")
     
 
 print("\nSensor class data: ", sensors)
