98351b3aa0eb758d3db1275ec554e5ca2ccb8ce6
Tilman <tlf.koenig@gmail.com>
2024-06-01 15:32:39 +0200

    sd

--- sensor_ini.py
+++ sensor_ini.py
@@ -1,6 +1,24 @@
 from  classes.humidity_sensor import humidity_sensor
+import json
 
 sensor = humidity_sensor(1, 0)
 print(sensor)
 
-sensor.calibrate();
+#sensor.calibrate();
+
+
+number_of_sensors = 1
+
+# Load sensor mappings:
+sensor_channel_mapping = json.load("bin\sensor_channel_mapping.json")
+print(sensor_channel_mapping)
+
+
+
+
+
+
+        
+
+
+
