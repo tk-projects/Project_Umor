059485285693feed20b6d47dfa44835a8b2ef366
Tilman <tlf.koenig@gmail.com>
2024-06-01 17:27:26 +0200

    sdas

--- sensor_main.py
+++ sensor_main.py
@@ -1,22 +1,19 @@
-def sensor_main():
-    from  classes.humidity_sensor import humidity_sensor
-    import os
-    import json
-    
-    sensors_file_path = os.path.join(os.getcwd() + "/bin/sensors.json")
-    print("Loading Sensor Data from: ", sensors_file_path)
+from  classes.humidity_sensor import humidity_sensor
+import os
+import json
 
-    with open(sensors_file_path, "r") as json_file:
-        sensor_json = json.load(json_file)
+sensors_file_path = os.path.join(os.getcwd() + "/bin/sensors.json")
+print("Loading Sensor Data from: ", sensors_file_path)
 
-    print("Loading succesful")
+with open(sensors_file_path, "r") as json_file:
+    sensor_json = json.load(json_file)
 
-    sensors = {}
+print("Loading succesful")
 
-    for sensor_name, sensor_json in sensor_json.items():
-        sensors[sensor_name] = humidity_sensor(sensor_json["sensor_id"], sensor_json["adc_channel"], sensor_json["name"], sensor_json["unit"], sensor_json["min_offset"],sensor_json["max_offset"])
-    
-    print("Sensor data: ", sensors)
+sensors = {}
 
-    return sensors
+for sensor_name, sensor_json in sensor_json.items():
+    sensors[sensor_name] = humidity_sensor(sensor_json["sensor_id"], sensor_json["adc_channel"], sensor_json["name"], sensor_json["unit"], sensor_json["min_offset"],sensor_json["max_offset"])
+
+print("Sensor data: ", sensors)
     
