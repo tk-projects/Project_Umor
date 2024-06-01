ee5dc399f224b6105612afa5af6d43b439ff2739
Tilman <tlf.koenig@gmail.com>
2024-06-01 16:28:52 +0200

    sd

--- sensor_ini.py
+++ sensor_ini.py
@@ -27,6 +27,7 @@ sensors = {}
 for i in range(1,number_of_sensors+1):
     sensor_idx = i;
     sensor_name = f"sensor_{i}";
+    print(sensor_name)
     sensor_channel = sensor_channel_mapping.get(sensor_name,"Sensor nicht gefunden. Prüfe die sensor_channel_mapping.json Datei im Ordner /bin/ !")
     sensors[sensor_name] = humidity_sensor(sensor_idx, sensor_channel, sensor_name, "% Lufteuchte")
 
