da99eb28fba39b94bebf9fd2bdc573aded45dbe1
Tilman <tlf.koenig@gmail.com>
2024-06-02 11:19:54 +0200

    sdas

--- functions/save_sensor_data.py
+++ functions/save_sensor_data.py
@@ -1,7 +1,13 @@
 import sqlite3
+import os
 
 def save_sensor_data(sensor_name, humidity_value):
-    conn = sqlite3.connect('sensor_data.db')
+    # Create the SQL directory if it doesn't exist
+    os.makedirs('SQL', exist_ok=True)
+
+    # Path to the database file in the SQL directory
+    db_path = os.path.join('SQL', 'sensor_data.db')
+    conn = sqlite3.connect(db_path)
     c = conn.cursor()
     
     c.execute("INSERT INTO humidity_data (sensor_name, humidity_value) VALUES (?, ?)", (sensor_name, humidity_value))
