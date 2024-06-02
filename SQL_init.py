9618b87ebb21f976a9728f21c8ad7b9010779af6
Tilman <tlf.koenig@gmail.com>
2024-06-02 12:37:24 +0200

    sda

--- SQL_init.py
+++ SQL_init.py
@@ -10,6 +10,15 @@ db_path = os.path.join('SQL', 'sensor_data.db')
 
 # Verbindung zur Datenbank herstellen
 conn = sqlite3.connect(db_path)
+
+# Establish connection to MySQL database
+conn = mysql.connector.connect(
+    host="umor",  # Replace with your MySQL host
+    user="tk",  # Replace with your MySQL username
+    password="1234",  # Replace with your MySQL password
+    database="sensor_data"  # Replace with the name of your MySQL database
+)
+
 c = conn.cursor()
 
 # Tabelle erstellen
