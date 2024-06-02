8e7204fb5f7fd701d93bc23675751b2e82bd9949
Tilman <tlf.koenig@gmail.com>
2024-06-02 13:52:21 +0200

    sdas

--- functions/flask_app.py
+++ functions/flask_app.py
@@ -10,15 +10,22 @@ def index():
     current_dir = os.path.dirname(os.path.abspath(__file__))
 
     # Path to the database file
-    db_path = os.path.join(current_dir, '..', 'SQL', 'sensor_data.db')
+    db_path = os.path.join(current_dir, 'SQL', 'sensor_data.db')
 
-
+    # Connect to the SQLite database
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
+
+    # Fetch data from the database
     cursor.execute('SELECT * FROM humidity_data')
     rows = cursor.fetchall()
+
+    # Close the database connection
     conn.close()
-    return render_template('/home/tk/Umor/Project_Umor/functions/templates/index.html', rows=rows)
+
+    # Pass the data to the template and render it
+    return render_template('/templates/sensor_data.html', rows=rows)
 
 if __name__ == '__main__':
+    # Run the Flask application
     app.run(host='0.0.0.0', port=80, debug=True)
