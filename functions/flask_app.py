976f08698b3e31d6b158dd5a4610687a39f7dab9
Tilman <tlf.koenig@gmail.com>
2024-06-02 13:54:47 +0200

    sdasd

--- functions/flask_app.py
+++ functions/flask_app.py
@@ -27,5 +27,4 @@ def index():
     return render_template('/templates/sensor_data.html', rows=rows)
 
 if __name__ == '__main__':
-    # Run the Flask application
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
