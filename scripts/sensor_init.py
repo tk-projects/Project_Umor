e533f8eac11bd703fe602b62c1c68cb30af64f20
Tilman <tlf.koenig@gmail.com>
2024-06-03 21:25:03 +0200

    sd

--- scripts/sensor_init.py
+++ scripts/sensor_init.py
@@ -2,7 +2,8 @@
 import sys
 import os
 
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
 from  classes.humidity_sensor import humidity_sensor
 from  classes.sensor_group import sensor_group
 from classes.sensor_cluster import sensor_cluster
