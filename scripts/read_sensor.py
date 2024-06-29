b7099b2996cced3fcbbc00b8bb67bddc3c970cba
Tilman <tlf.koenig@gmail.com>
2024-06-29 09:51:26 +0200

    sd

--- scripts/read_sensor.py
+++ scripts/read_sensor.py
@@ -16,12 +16,3 @@ def read_sensor(sensor_id=None,sensor_name=None):
     print(sensor_id)
     sensor =  get_sensor(sensor_id)
     print(sensor)
-
-if __name__ == "__main__":
-    # Check if arguments are passed
-    if len(sys.argv) > 1:
-        # Parse sensor_id from command line arguments
-        sensor_id = int(sys.argv[1])  # Convert the argument to an integer
-        read_sensor(sensor_id=sensor_id)
-    else:
-        print("Please provide a sensor_id as an argument.")
