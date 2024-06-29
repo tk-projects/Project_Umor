2bd6ae1c4605c2185b649976fd3d832ab9e0d026
Tilman <tlf.koenig@gmail.com>
2024-06-29 09:45:44 +0200

    Added sensor read script for easy measurement

---
+++ scripts/read_sensor.py
@@ -0,0 +1,17 @@
+# Read sensor data script for debugging
+import os
+import sys
+
+
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+# Add the parent directory to the sys.path
+sys.path.append(parent_dir)
+
+from classes.humidity_sensor import humidity_sensor
+from functions.get_sensor import get_sensor
+
+
+
+def read_sensor(sensor_id=None,sensor_name=None):
+    sensor =  get_sensor(sensor_id)
+    print(sensor)
