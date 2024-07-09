3613d0403c8d6a9f8bab6df14054e0d0b061f4a3
Tilman <tlf.koenig@gmail.com>
2024-07-09 19:47:18 +0200

    sd

--- scripts/__main__.py
+++ scripts/__main__.py
@@ -1,7 +1,21 @@
+
+import argparse
 from view_database import print_database
 from read_sensor import read_sensor
 
-if __name__ == "__main__":
-    print_database()
+def main():
+    parser = argparse.ArgumentParser(description="Execute specific functions.")
+    parser.add_argument('--print_database', action='store_true', help="Print the database content")
+    parser.add_argument('--read_sensor', action='store_true', help="Read sensor data")
+    
+    args = parser.parse_args()
 
-    read_sensor()
+    if args.print_database:
+        print_database()
+    
+    if args.read_sensor:
+        read_sensor()
+
+
+if __name__ == "__main__":
+    main()
